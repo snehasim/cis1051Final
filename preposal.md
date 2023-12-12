e3af4cfa14a74d87e4926266f1cd2c69a579336e
snehasim <130010609+snehasim@users.noreply.github.com>
2023-12-12 00:13:18 -0500

    Add files via upload

--- preposal.md
+++ preposal.md
@@ -1,12 +1,13 @@
 # Preproposal
 
 ## What idea(s) do you have for your final project?
-Music matching website
+
+TODO
 
 ## If you plan to collaborate with one or two classmates, what are their names?
 
-Just me
+TODO
 
 ## Do you have any questions of your own?
 
-
+TODO
---
+++ songlist.py
@@ -0,0 +1,19 @@
+ninetiesRANDB = ["https://open.spotify.com/track/1KGi9sZVMeszgZOWivFpxs", "https://open.spotify.com/track/2VjXGuPVVxyhMgER3Uz2Fe", "https://open.spotify.com/track/3XKIUb7HzIF1Vu9usunMzc", "https://open.spotify.com/track/6sHsXIJoEN5JpdkGMQDJxt", "https://open.spotify.com/track/3l3xTXsUXeWlkPqzMs7mPD"]
+thousandsRANDB = ["https://open.spotify.com/track/49FYlytm3dAAraYgpoJZux", "https://open.spotify.com/track/0W4NhJhcqKCqEP2GIpDCDq", "https://open.spotify.com/track/0DLOyyQvwPTSDKuhpzMMwA", "https://open.spotify.com/track/7pv80uUHfocFqfTytu1MVi", "https://open.spotify.com/track/4x2YdtTppdbL4kHPOjaunq"]
+tensRANDB = ["https://open.spotify.com/track/3xKsf9qdS1CyvXSMEid6g8", "https://open.spotify.com/track/0KKkJNfGyhkQ5aFogxQAPU", "https://open.spotify.com/track/0f8eRy9A0n6zXpKSHSCAEp", "https://open.spotify.com/track/1eUGmzzvahJjOSWgDHuRlv", "https://open.spotify.com/track/5rgrBsAFYMun6yhtnLKRPz"]
+currentRANDB = ["https://open.spotify.com/track/3fDDXsDuxBLinRP4Rw8pMc", "https://open.spotify.com/track/27d6hPrn21lrr9l91d4L4I", "https://open.spotify.com/track/4hbadZZrbM08KZIQZomWaI", "https://open.spotify.com/track/1u9OkMaHqaqt4cyRqJEH0v", "https://open.spotify.com/track/4fcGgxx1moBvJZDgRU0hTk"] 
+
+ninetiesPop = ["https://open.spotify.com/track/7sjwaBQ6M3aVigYfBjm1sL"]
+thousandsPop = ["https://open.spotify.com/track/7sjwaBQ6M3aVigYfBjm1sL"]
+tensPop = ["https://open.spotify.com/track/7sjwaBQ6M3aVigYfBjm1sL"]
+currentPop = ["https://open.spotify.com/track/7sjwaBQ6M3aVigYfBjm1sL"] 
+
+ninetiesHipHop = ["https://open.spotify.com/track/503OTo2dSqe7qk76rgsbep", "https://open.spotify.com/track/7KwZNVEaqikRSBSpyhXK2j", "https://open.spotify.com/track/33ZXjLCpiINn8eQIDYEPTD", "https://open.spotify.com/track/0uEp9E98JB5awlA084uaIg", "https://open.spotify.com/track/119c93MHjrDLJTApCVGpvx"]
+thousandsHipHop = ["https://open.spotify.com/track/6ScJMrlpiLfZUGtWp4QIVt", "https://open.spotify.com/track/7m97yKVzPpXV9Z4ezEziOD", "https://open.spotify.com/track/2q4rjDy9WhaN3o9MvDbO21", "https://open.spotify.com/track/4RY96Asd9IefaL3X4LOLZ8", "https://open.spotify.com/track/1PS1QMdUqOal0ai3Gt7sDQ"]
+tensHipHop = ["https://open.spotify.com/track/7sjwaBQ6M3aVigYfBjm1sL"]
+currentHipHop = ["https://open.spotify.com/track/7sjwaBQ6M3aVigYfBjm1sL"] 
+
+ninetiesIndie = ["https://open.spotify.com/track/6L89mwZXSOwYl76YXfX13s", "https://open.spotify.com/track/1Cwsd5xI8CajJz795oy4XF", "https://open.spotify.com/track/6txWz9UapYHVxEd7dDIHXT", "https://open.spotify.com/track/1pKYYY0dkg23sQQXi0Q5zN", "https://open.spotify.com/track/1UVll7jjUlqSGxCZ6uXVaD"]
+thousandsIndie = ["https://open.spotify.com/track/1SvIUtl5QuOTJhKNVikt6m", "https://open.spotify.com/track/3DQVgcqaP3iSMbaKsd57l5", "https://open.spotify.com/track/0DiWol3AO6WpXZgp0goxAV", "https://open.spotify.com/track/3kMrazSvILsgcwtidZd1Qd", "https://open.spotify.com/track/5UBBJnFxsfjFxfrtvErQoH"]
+tensIndie = ["https://open.spotify.com/track/7w87IxuO7BDcJ3YUqCyMTT", "https://open.spotify.com/track/0XN5vm980boRCELIwD2CIe", "https://open.spotify.com/track/7B3z0ySL9Rr0XvZEAjWZzM", "https://open.spotify.com/track/0q6LuUqGLUiCPP1cbdwFs3", "https://open.spotify.com/track/0GO8y8jQk1PkHzS31d699N"]
+currentIndie = ["https://open.spotify.com/track/2NUIlCRqz6OxgGOipYqBCw", "https://open.spotify.com/track/0f4motUd4Dn7UXr6I7GUsG", "https://open.spotify.com/track/5QXO4uPk1FdznZEGqTJsLb", "https://open.spotify.com/track/1cJuew45cBdyPnXyBipX17", "https://open.spotify.com/track/5zhjQ18PepzPDl9uTz2NH2"]
